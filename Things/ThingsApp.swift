0eea4af39480278d5a23b5f8176ab2375afde36b
d1scal <evgeniygroza@discal.ru>
2026-05-17 01:50:33 +0300

    Initial Commit

---
+++ Things/ThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThingsApp.swift
+//  Things
+//
+//  Created by Евгений Гроза on 17.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
